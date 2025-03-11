0b8c91b91ea8e178e340a881ceeca3326543c280
Jermaine Williams <wjermaine608@gmail.com>
2025-03-11 00:56:04 +0000

    Need to Combine

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Home = () => {
 	return (
 		<div className="text-center">
 			{/*  navbar */}
-			
+			<Navbar title={"AniMedia"} />
 			{/*  Carousel */}
 
 			{/*  jumbotron */}
