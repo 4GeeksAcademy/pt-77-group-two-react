89f9dfed0d14bae67292a547f9743d6943a517c3
Evens Antoine <evens7antoine@gmail.com>
2025-03-11 00:56:08 +0000

    my Footer

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,15 +4,27 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			{/*  navbar */}
-			
+
 			{/*  Carousel */}
 
 			{/*  jumbotron */}
-			
+
 			{/*  cards */}
-			
+
 			{/*  footer */}
-			
+			<div className="card text-center">
+				<div className="card-header">
+					Featured
+				</div>
+				<div className="card-body">
+					<h5 className="card-title">Group Two "Landing Page with React" Project</h5>
+					<p className="card-text">Made by Evens A. , Byron R. , Joseph D. , and Jermaine W.</p>
+					<a href="#" className="btn btn-primary">4Geeks</a>
+				</div>
+				<div className="card-footer text-body-secondary">
+					2 days ago
+				</div>
+			</div>
 		</div>
 	);
 };
